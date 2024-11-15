dc10cf3644e86a271c9ebf32809e47941a2d3a55
Tom Bihoreau <82575721+tombihoreau@users.noreply.github.com>
2024-11-15 12:05:04 +0100

    commit

---
+++ requests/index.js
@@ -0,0 +1,35 @@
+
+
+
+//Import the sequelize library
+const { QueryTypes } = require('sequelize');
+
+
+/* 3 cas d'utilisations des requetes préparés */
+
+
+// Cas d'utilisation où on utilise un tableau en paramètre 
+await sequelize.query('SELECT * FROM projects WHERE status = ?', {
+  replacements: ['active'],
+  type: QueryTypes.SELECT,
+});
+
+
+// Cas d'utilisation où on utilise une clé en paramètre 
+await sequelize.query('SELECT * FROM projects WHERE status = :status', {
+  replacements: { status: 'active' },
+  type: QueryTypes.SELECT,
+});
+
+//
+await sequelize.query('UPDATE projects SET updatedAt = :updatedAt WHERE id = :id', {
+    replacements: { updatedAt, id },
+  });
+
+// Cas d'utilisation où on utilise plusieurs valeurs en  paramètre 
+
+await sequelize.query('SELECT * FROM projects WHERE status IN(:status)', {
+  replacements: { status: ['active', 'inactive'] },
+  type: QueryTypes.SELECT,
+});
+
